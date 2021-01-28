a89565a19ad93a3d9e7ea111492360657e8df8fd
Harman Tiwana <47931837+HarmanTiwana901@users.noreply.github.com>
2021-01-27 19:00:01 -0800

    Add files via upload

---
+++ assignment2_1303/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+
+class Assignment2
+{
+
+    public bool validateIPAddress(string ip)
+    {
+        IPAddress ipParsed;
+        bool ValidateIP = IPAddress.TryParse(ip, out ipParsed);
+        if (ValidateIP)
+        {
+            Console.WriteLine("Valid ip address");
+            return true;
+        } else
+        {
+            Console.WriteLine("Invalid ip address");
+            return false;
+        }
+           
+       
+            
+        
+    }
+
+   /*
+    public bool ValidateIPAddress(String strIP)
+    {
+
+        try
+        {
+            char chrSeperate = '.'; // split
+            string[] arrLens = strIP.Split(chrSeperate);
+
+
+            if (arrLens.Length != 4)
+            {
+                return false;
+            }
+
+            Int32 temp;
+            Int16 MAXVALUE = 255; // const for max length, makes sure user doesnt input a string thats too long
+
+            foreach (String strLen in arrLens)
+            {
+                if (strLen.Length > 3)
+                {
+                    return false;
+                }
+
+                temp = int.Parse(strLen);
+                if (temp > MAXVALUE)
+                {
+                    return false;
+                }
+            }
+            return true;
+        } catch(FormatException e)
+        {
+            Console.Write("Error");
+            return false;
+        }
+       
+    }
+   */
+}
+
+class Program
+{
+    static void Main()
+    {
+        Assignment2 validate = new Assignment2();
+        //Console.WriteLine(validate.ValidateIPAddress("192.168.56.1"));
+
+        //var ipParts = new Byte[] { 101, 102, 103, 0 };
+        var checker = validate.validateIPAddress("192.168.56.1");
+
+        Console.WriteLine("IP Address:{0}", checker);
+       
+    }
+}
