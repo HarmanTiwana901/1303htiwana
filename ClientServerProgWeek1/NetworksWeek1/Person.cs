71d9a639cd83775cde3e2c22f62687b252337c8c
Harman Tiwana <47931837+HarmanTiwana901@users.noreply.github.com>
2021-01-27 20:59:47 -0800

    Add files via upload

---
+++ ClientServerProgWeek1/NetworksWeek1/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientServerProgWeek1.NetworksWeek1
+{
+    class Person
+    {
+        public string Name;
+        public string Lastname;
+        public int Age;
+        public string Address;
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name, string lastName)
+        {
+
+        }
+    }
+}
